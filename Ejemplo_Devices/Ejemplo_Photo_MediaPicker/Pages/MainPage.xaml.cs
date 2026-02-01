6e14cad88af8c615541f05b38b956852e1559b6e
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-02-01 11:55:17 -0300

    agregue el mediapicker

---
+++ Ejemplo_Devices/Ejemplo_Photo_MediaPicker/Pages/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejemplo_Photo_MediaPicker.Pages;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    async private void OnAbrirCamaraClicked(object? sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+            if (photo != null)
+            {
+                using Stream sourceStream = await photo.OpenReadAsync();
+                var image = new Image { Source = ImageSource.FromStream(() => sourceStream) };
+                ImgPhoto.Source = image.Source;
+            }
+        }
+    }
+}
---
+++ Ejemplo_Devices/Ejemplo_Photo_MediaPicker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ejemplo_Photo_MediaPicker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
