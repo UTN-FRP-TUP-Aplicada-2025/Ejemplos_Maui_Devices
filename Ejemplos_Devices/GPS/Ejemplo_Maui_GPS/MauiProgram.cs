5d87dae571f69cf3695d6657d627f8cd204fb709
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-04-15 09:27:02 -0300

    GPS build

--- Ejemplos_Devices/GPS/Ejemplo_Maui_GPS/MauiProgram.cs
+++ Ejemplos_Devices/GPS/Ejemplo_Maui_GPS/MauiProgram.cs
@@ -12,7 +12,7 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .AddServices()
             .ConfigureFonts(fonts =>
-            {
+            { 
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
