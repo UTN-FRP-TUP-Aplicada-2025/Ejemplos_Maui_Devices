6b3d20c6f674545ca367192401af85976c277768
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-05-18 12:46:05 -0300

    probando xcode26.3 -100

--- Ejemplos_Devices/Phone/Ejemplo_Maui_Dialer/Platforms/iOS/AppDelegate.cs
+++ Ejemplos_Devices/Phone/Ejemplo_Maui_Dialer/Platforms/iOS/AppDelegate.cs
@@ -3,7 +3,7 @@
 namespace Ejemplo_Maui_Dialer
 {
     [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate
+    public class AppDelegate : MauiUIApplicationDelegate 
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
