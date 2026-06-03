7f236c1c2fd3220acc3fd03ded290b8c8cafbe1a
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-06-03 08:36:51 -0300

    prueba gps en ios - overlay resuelto 2

--- Ejemplos_Devices/Integrada/Ejemplo_Maui_Hibrida/Pages/MainPage.xaml.cs
+++ Ejemplos_Devices/Integrada/Ejemplo_Maui_Hibrida/Pages/MainPage.xaml.cs
@@ -12,9 +12,11 @@ public partial class MainPage : ContentPage
         BindingContext = mainViewModel;
 
         // URL con geo=1 para que el flujo del overlay se dispare al iniciar.
-        //mainViewModel.Url = "https://geolocate.somee.com/geolocate?geo=1";
+        mainViewModel.Url = "https://geolocate.somee.com/geolocate?geo=1";
         //mainViewModel.Url = "https://www.google.com";
-        mainViewModel.Url = "https://nvtc4bdq-8080.brs.devtunnels.ms/?geo=1";
+        //mainViewModel.Url = "https://nvtc4bdq-8080.brs.devtunnels.ms/?geo=1";
+
+        mainViewModel.Url = "https://geolocate.somee.com";
 
         mainViewModel.MostrarNavegador = true;
     }
