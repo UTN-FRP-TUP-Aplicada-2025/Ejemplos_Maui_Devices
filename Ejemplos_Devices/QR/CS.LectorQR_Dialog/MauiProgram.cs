31754447069e4ceaec14b9a092609e581b445b50
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-06-29 20:26:17 -0300

    camerascanner.maui.dialog ios

--- Ejemplos_Devices/QR/CS.LectorQR_Dialog/MauiProgram.cs
+++ Ejemplos_Devices/QR/CS.LectorQR_Dialog/MauiProgram.cs
@@ -17,7 +17,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("MaterialIconsOutlined-Regular.otf", "MaterialIconsOutlined");
             })
-        #region barcode scanner
+        #region QR
             .UseCameraScanner();
         #endregion
 
